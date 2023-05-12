8d79e5426436b15b961b2160c5c9711ba5718722
Ntholi Nkhatho <ntholi@live.com>
2023-05-12 10:03:48 +0200

    setup mantine color scheme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,30 @@
+import NextApp, { AppProps, AppContext } from 'next/app';
 import Head from 'next/head';
-import { MantineProvider } from '@mantine/core';
+import {
+  ColorScheme,
+  ColorSchemeProvider,
+  MantineProvider,
+} from '@mantine/core';
+import { Notifications } from '@mantine/notifications';
+import { useState } from 'react';
+import { getCookie, setCookie } from 'cookies-next';
+import { ModalsProvider } from '@mantine/modals';
 import '@/styles/globals.css';
-import { AppProps } from 'next/app';
 
-export default function App(props: AppProps) {
+export default function App(props: AppProps & { colorScheme: ColorScheme }) {
   const { Component, pageProps } = props;
+  const [colorScheme, setColorScheme] = useState<ColorScheme>(
+    props.colorScheme
+  );
+
+  const toggleColorScheme = (value?: ColorScheme) => {
+    const nextColorScheme =
+      value || (colorScheme === 'dark' ? 'light' : 'dark');
+    setColorScheme(nextColorScheme);
+    setCookie('mantine-color-scheme', nextColorScheme, {
+      maxAge: 60 * 60 * 24 * 30,
+    });
+  };
 
   return (
     <>
@@ -22,16 +42,29 @@ export default function App(props: AppProps) {
         />
       </Head>
 
-      <MantineProvider
-        withGlobalStyles
-        withNormalizeCSS
-        theme={{
-          /** Put your mantine theme override here */
-          colorScheme: 'light',
-        }}
+      <ColorSchemeProvider
+        colorScheme={colorScheme}
+        toggleColorScheme={toggleColorScheme}
       >
-        <Component {...pageProps} />
-      </MantineProvider>
+        <MantineProvider
+          theme={{ colorScheme }}
+          withGlobalStyles
+          withNormalizeCSS
+        >
+          <Notifications />
+          <ModalsProvider>
+            <Component {...pageProps} />
+          </ModalsProvider>
+        </MantineProvider>
+      </ColorSchemeProvider>
     </>
   );
 }
+
+App.getInitialProps = async (appContext: AppContext) => {
+  const appProps = await NextApp.getInitialProps(appContext);
+  return {
+    ...appProps,
+    colorScheme: getCookie('mantine-color-scheme', appContext.ctx) || 'light',
+  };
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
-import { Button, Center, Flex } from '@mantine/core';
+import { Button, Center, Flex, useMantineColorScheme } from '@mantine/core';
 
 export default function Home() {
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
   return (
     <>
       <main>
         <Flex h='100vh' justify='center' align='center'>
-          <Button>FiveDays</Button>
+          <Button onClick={() => toggleColorScheme()}>FiveDays</Button>
         </Flex>
       </main>
     </>
