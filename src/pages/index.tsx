32b8a415c1a27c716952e05ad629d2ad5a2427d0
Ntholi Nkhatho <ntholi@live.com>
2023-05-12 13:36:07 +0200

    delete login page and use 'required: true' in session

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import { Button, Center, Flex, useMantineColorScheme } from '@mantine/core';
+import { useSession } from 'next-auth/react';
 
 export default function Home() {
   const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+  const { data: session, status } = useSession({
+    required: true,
+  });
   return (
     <>
       <main>
