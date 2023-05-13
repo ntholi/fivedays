3c7e8398553ce8de4da327ef4253882d0fc15a59
Ntholi Nkhatho <ntholi@live.com>
2023-05-13 11:53:15 +0200

    send next-auth accessToken back to client

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -6,8 +6,37 @@ const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+      authorization: {
+        url: 'https://accounts.google.com/o/oauth2/v2/auth',
+        params: {
+          prompt: 'consent',
+          access_type: 'offline',
+          response_type: 'code',
+          scope:
+            'openid https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/classroom.courses.readonly',
+        },
+      },
     }),
   ],
   secret: process.env.JWT_SECRET,
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token = Object.assign({}, token, {
+          accessToken: account.access_token,
+        });
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (session) {
+        session = Object.assign({}, session, {
+          accessToken: token.accessToken,
+        });
+      }
+      return session;
+    },
+  },
 };
+
 export default NextAuth(authOptions);
