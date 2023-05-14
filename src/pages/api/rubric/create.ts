022227e0e64b8f932a0866a644287ad0160e0fb2
Ntholi Nkhatho <ntholi@live.com>
2023-05-14 20:28:15 +0200

    test OpenAIApi for creating rubrics

--- src/pages/api/rubric/create.ts
+++ src/pages/api/rubric/create.ts
@@ -1,15 +1,33 @@
 import { classroom_v1 } from 'googleapis';
 import { NextApiRequest, NextApiResponse } from 'next';
+import { Configuration, OpenAIApi } from 'openai';
 
 type Data = {
   courseWork: classroom_v1.Schema$CourseWork;
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
   const { courseWork } = req.body;
-  console.log(courseWork);
+
+  const configuration = new Configuration({
+    organization: process.env.OPENAI_ORG_ID,
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+  const response = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    messages: [
+      {
+        role: 'user',
+        content: `Create a marking rubric based on the following question: "${courseWork.description}"`,
+      },
+    ],
+  });
+
+  console.log(response.data.choices[0].message);
+
   res.status(200).json(courseWork);
 }
