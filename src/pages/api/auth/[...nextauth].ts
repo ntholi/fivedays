ba7e33b4b07513a6269974e1d2eb923b069822ad
Ntholi Nkhatho <ntholi@live.com>
2023-05-19 08:21:47 +0200

    setup refreshAccessToken

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
 import NextAuth, { NextAuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import axios from 'axios';
 
 const authOptions: NextAuthOptions = {
   providers: [
@@ -25,7 +26,29 @@ const authOptions: NextAuthOptions = {
       if (account) {
         token = Object.assign({}, token, {
           accessToken: account.access_token,
+          refreshToken: account.refresh_token,
+          accessTokenExpires: Date.now() + account.expires_in * 1000,
         });
+      } else if (Date.now() > token.accessTokenExpires) {
+        // accessToken has expired, try to refresh it
+        const refreshTokenResponse = await refreshAccessToken(
+          token.refreshToken
+        );
+        if (refreshTokenResponse.access_token) {
+          token = Object.assign({}, token, {
+            accessToken: refreshTokenResponse.access_token,
+            refreshToken: refreshTokenResponse.refresh_token
+              ? refreshTokenResponse.refresh_token
+              : token.refreshToken, // new refresh token is only provided if the old one has expired
+            accessTokenExpires:
+              Date.now() + refreshTokenResponse.expires_in * 1000,
+          });
+        } else {
+          console.log(
+            'Failed to refresh access token: ' +
+              JSON.stringify(refreshTokenResponse)
+          );
+        }
       }
       return token;
     },
@@ -33,11 +56,33 @@ const authOptions: NextAuthOptions = {
       if (session) {
         session = Object.assign({}, session, {
           accessToken: token.accessToken,
+          refreshToken: token.refreshToken,
+          accessTokenExpires: token.accessTokenExpires,
         });
       }
       return session;
     },
   },
 };
+
+async function refreshAccessToken(refreshToken: any) {
+  try {
+    const { data } = await axios({
+      method: 'post',
+      url: 'https://oauth2.googleapis.com/token',
+      params: {
+        client_id: process.env.GOOGLE_CLIENT_ID,
+        client_secret: process.env.GOOGLE_CLIENT_SECRET,
+        grant_type: 'refresh_token',
+        refresh_token: refreshToken,
+      },
+    });
+
+    return data;
+  } catch (error: any) {
+    console.log('Error refreshing access token: ', error.response.data);
+    return error.response.data;
+  }
+}
 
 export default NextAuth(authOptions);
