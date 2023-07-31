f3899c6d02ffef72cebafab97ae2b4321c7a4fe9
Ntholi Nkhatho <ntholi@live.com>
2023-07-31 15:31:26 +0200

    delete everything

--- src/lib/helpers/prompts/rubric.ts
+++
@@ -1,4 +0,0 @@
-export function createRubricPrompt(question: string, points: number) {
-  return `Create rubric from question, with ${points} points: "${question}"
-  return valid JSON in Format: [{points: int, title: string, description: string}, {...]`;
-}
--- src/lib/models/rubric.ts
+++
@@ -1,7 +0,0 @@
-interface Rubric {
-  id?: number;
-  questionId?: string | null;
-  title: string;
-  description: string;
-  points: number;
-}
