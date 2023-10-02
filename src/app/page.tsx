104890bea97aa74a72d9e4395e9e124c9068edb5
Ntholi Nkhatho <ntholi@live.com>
2023-10-02 13:32:33 +0200

    setup mantine

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import Image from 'next/image';
+import { Button, Stack, Title } from '@mantine/core';
 
 export default function Home() {
   return (
-    <main>
-      <h1>Hello World</h1>
+    <main className='flex h-screen justify-center items-center'>
+      <Stack>
+        <Title className='font-normal'>Hello World</Title>
+        <Button>Click Me</Button>
+      </Stack>
     </main>
   );
 }
