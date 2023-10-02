b89afd1dc2a8cd6c8d075086c2337c630cdfe737
Ntholi Nkhatho <ntholi@live.com>
2023-10-02 16:18:54 +0200

    setup next-nprogress-bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 // Import styles of packages that you've installed.
 // All packages except `@mantine/hooks` require styles imports
 import '@mantine/core/styles.css';
-
 import { MantineProvider, ColorSchemeScript } from '@mantine/core';
 import { Metadata } from 'next';
 import Navbar from './core/Navbar';
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,17 @@
 'use client';
 import { SessionProvider } from 'next-auth/react';
+import { AppProgressBar as ProgressBar } from 'next-nprogress-bar';
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider>
+      {children}
+      <ProgressBar
+        height='3px'
+        color='#2196F3'
+        options={{ showSpinner: false }}
+        shallowRouting
+      />
+    </SessionProvider>
+  );
 }
