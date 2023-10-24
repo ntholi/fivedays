70f8e7bfd9068c2dafe006ac1ff8df1afc1a5c6d
Ntholi Nkhatho <ntholi@live.com>
2023-10-24 09:12:17 +0200

    simplify getting students and coursework logic

--- src/app/courses/[courseId]/work/[courseWorkId]/submissions/page.tsx
+++ src/app/courses/[courseId]/work/[courseWorkId]/submissions/page.tsx
@@ -1,8 +1,9 @@
-import { Anchor, Breadcrumbs, Text } from '@mantine/core';
+import { Anchor, Breadcrumbs } from '@mantine/core';
 import React, { Suspense } from 'react';
 import CourseLink from '../heading/CourseLink';
 import Link from 'next/link';
 import googleClassroom from '@/lib/config/googleClassroom';
+import { classroom_v1 } from 'googleapis';
 
 type Props = {
   params: {
@@ -20,10 +21,32 @@ const getCourseWork = async (courseId: string, courseWorkId: string) => {
   return courseWork;
 };
 
+const getSubmissions = async (courseId: string, courseWorkId: string) => {
+  const classroom = await googleClassroom();
+  const { data } = await classroom.courses.courseWork.studentSubmissions.list({
+    courseId,
+    courseWorkId,
+    states: ['CREATED', 'TURNED_IN', 'RETURNED'],
+  });
+  return data.studentSubmissions;
+};
+
+const getStudents = async (courseId: string) => {
+  const classroom = await googleClassroom();
+  const { data } = await classroom.courses.students.list({
+    courseId,
+  });
+  return data.students;
+};
+
 export default async function SubmissionsPage({
   params: { courseId, courseWorkId },
 }: Props) {
   const courseWork = await getCourseWork(courseId, courseWorkId);
+
+  const submissions = await getSubmissions(courseId, courseWorkId);
+  const students = await getStudents(courseId);
+
   const items = [
     <Anchor component={Link} href={'/courses'} key={1}>
       Courses
@@ -39,9 +62,13 @@ export default async function SubmissionsPage({
       {courseWork.title}
     </Anchor>,
   ];
+
   return (
     <>
       <Breadcrumbs>{items}</Breadcrumbs>
+      {students?.map((it) => (
+        <h1 key={it.userId}>{it.profile?.name?.fullName}</h1>
+      ))}
     </>
   );
 }
