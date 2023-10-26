1e895a2ae125aa15907fbb221754acb577a8b00b
Ntholi Nkhatho <ntholi@live.com>
2023-10-26 08:33:00 +0200

    Display descriptions and markers on the criteria

--- src/app/courses/[courseId]/work/[courseWorkId]/submissions/Grader.tsx
+++ src/app/courses/[courseId]/work/[courseWorkId]/submissions/Grader.tsx
@@ -1,6 +1,14 @@
 'use client';
 import { RootState } from '@/lib/redux/store';
-import { Accordion, Flex, Group, Slider, Stack, Text } from '@mantine/core';
+import {
+  Accordion,
+  Flex,
+  Group,
+  Slider,
+  Stack,
+  Text,
+  Textarea,
+} from '@mantine/core';
 import { Criterion } from '@prisma/client';
 import axios from 'axios';
 import { useSession } from 'next-auth/react';
@@ -17,6 +25,16 @@ type AccordionLabelProps = {
 };
 
 function AccordionLabel({ title, points }: AccordionLabelProps) {
+  const generateMarks = (points: number) => {
+    const step = points / 4;
+    const marks = [];
+    for (let i = 0; i <= points; i += step) {
+      const val = i.toFixed(2);
+      marks.push({ value: Number(val), label: String(val) });
+    }
+    return marks;
+  };
+
   return (
     <Stack h={80} mr={'sm'}>
       <Flex justify='space-between'>
@@ -28,12 +46,9 @@ function AccordionLabel({ title, points }: AccordionLabelProps) {
       <Slider
         color='blue'
         size='sm'
+        max={points}
         onClick={(e) => e.stopPropagation()}
-        marks={[
-          { value: 20, label: '20%' },
-          { value: 50, label: '50%' },
-          { value: 80, label: '80%' },
-        ]}
+        marks={generateMarks(points)}
       />
     </Stack>
   );
@@ -51,7 +66,12 @@ export default function Grader({ criteria }: Props) {
         <AccordionLabel {...it} />
       </Accordion.Control>
       <Accordion.Panel>
-        <Text size='sm'>Comments Here</Text>
+        <Stack>
+          <Textarea description='Comments' />
+          <Text size='xs' c='dimmed'>
+            {it.description}
+          </Text>
+        </Stack>
       </Accordion.Panel>
     </Accordion.Item>
   ));
